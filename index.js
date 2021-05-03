111af5c539d1585d4522a0e73e301682493fb69b
Giuliano Crespe <gcrespe3@hotmail.com>
2021-05-03 20:47:30 -0300

    created home files, cant make it render

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use-strict';
 
-const { app, BrowserWindow, ipcMain } = require('electron'); 
+const { app, BrowserWindow } = require('electron'); 
 
 const path = require('path')
  
@@ -14,7 +14,7 @@ app.on('ready' , () => {
       height: 700,
       maxWidth: 1200,
       maxHeight: 700,
-      frame: false, //tirar a parte de cima (incluindo botoes, so fecha pelo terminal)
+      frame: true, //tirar a parte de cima (incluindo botoes, so fecha pelo terminal)
       webPreferences: {
         nodeIntegration: true
     }
@@ -27,15 +27,3 @@ app.on('ready' , () => {
 app.on('window-all-closed', () => {
   app.quit();
 })
-
-ipcMain.on('about-window', () => {
-  
-  let aboutWindow = new BrowserWindow({
-    width: 1200,
-    height: 700,
-    frame: true, 
-  });
-
-  aboutWindow.loadURL(`file://${__dirname}/app/about/about.html`);
-
-});
